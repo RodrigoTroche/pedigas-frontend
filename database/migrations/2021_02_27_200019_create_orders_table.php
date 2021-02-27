9fc118547cfe00634fdf740e1bb7f3ee73843ef2
Rodrigo Troche <rodrigo.troche15@gmail.com>
2021-02-27 17:14:37 -0300

    first commit

---
+++ database/migrations/2021_02_27_200019_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('payment_method_id');
+            $table->unsignedBigInteger('shipping_address_id');
+            $table->text('user_raw_data');
+            $table->integer('amount');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
